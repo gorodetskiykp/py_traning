d833ea79ff983a61dd8319ab5e5e1fdab0125a58
GorodetskiyKP <gorodetskiykp@gmail.com>
2022-09-27 13:35:42 +0300

    Добавил 4 урок по aiohttp

---
+++ aiohttp/mikhail_kashkin_tutorial/004_connect_to_db/demo/app.py
@@ -0,0 +1,25 @@
+from aiohttp import web
+import jinja2
+import aiohttp_jinja2
+import asyncpgsa
+
+from .routes import setup_routes
+
+
+async def create_app(config: dict):
+    app = web.Application()
+    app['config'] = config
+    aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('demo', 'templates'))
+    setup_routes(app)
+    app.on_startup.append(on_start)
+    app.on_cleanup.app(on_shatdown)
+    return app
+
+
+async def on_start(app):
+    config = app['config']
+    app['db'] = await asyncpgsa.create_pool(dsn=config['database_uri'])
+
+
+async def on_shatdown(app):
+    await app['db'].close()
